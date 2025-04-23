7613f1a831bc3e71594e4ae69d0beac895bb2b75
Bahm1979 <epnmla@gmail.com>
2025-04-23 21:46:32 +0200

    ✅ Ajout SSL dans server.js pour Render

--- server.js
+++ server.js
@@ -55,11 +55,15 @@ const upload = multer({
 });
 
 const dbConfig = {
-  host: process.env.DB_HOST || 'localhost',
-  user: process.env.DB_USER || 'root',
-  password: process.env.DB_PASSWORD || '',
-  database: process.env.DB_NAME || 'gestion_locative_v3'
-};
+    host: process.env.DB_HOST,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME,
+    ssl: {
+      rejectUnauthorized: true
+    }
+  };
+  
 
 async function connectDB() {
   try {
